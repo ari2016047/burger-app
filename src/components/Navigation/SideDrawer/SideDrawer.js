fbf7f3552455face616eaaa3807155806fa9bf82
ari201604d7 <2016047@iiitdmj.ac.in>
2020-05-29 21:55:11 +0530

    SideDrawer

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import BackDrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxa';
+
+const sideDrawer = (props) =>{
+
+    return (
+        <Aux>
+            <BackDrop show={true}/>
+            <div className={classes.Drawer}>
+                <Logo height="10%"/>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
+}
+
+export default sideDrawer;
